c217894541f6d4b936132e918266e2378d2bbfc0
Jesse Nauman <jnauman619@gmail.com>
2021-01-19 13:23:18 -0600

    Make calculator work with single operator

--- app.js
+++ app.js
@@ -26,3 +26,62 @@ function operate(operator, a, b) {
     }
 }
 
+let display = document.getElementById('display');
+let displayValue = display.textContent;
+let numHasBeenClicked = false;
+let firstValue = 0;
+let secondValue = 0;
+let operator;
+let opHasBeenClicked;
+
+const numberBtns = Array.from(document.getElementsByClassName('num'));
+numberBtns.forEach((button) => {
+    button.addEventListener('click', () => {
+        let thisButtonValue = button.value;
+        if (numHasBeenClicked === false) {
+            display.textContent = thisButtonValue;
+        } else if (numHasBeenClicked === true) {
+            display.textContent = display.textContent + thisButtonValue;
+        }
+        numHasBeenClicked = true;
+    });
+});
+
+const operatorBtns = Array.from(document.getElementsByClassName('operator'));
+operatorBtns.forEach((button) => {
+    button.addEventListener('click', () => {
+        opHasBeenClicked = true;
+        operator = button.value;
+        firstValue = parseInt(display.textContent);
+        console.log("OP firstValue is " + firstValue);
+        numHasBeenClicked = false;
+    });
+
+    //event listener for equal button to run operate function and return answer to display
+});
+
+//check if opHasBeenClicked is true
+const equalsBtn = document.querySelector('#equals');
+equalsBtn.addEventListener('click', () => {
+    secondValue = parseInt(display.textContent);
+    console.log("EQUALS firstValue is " + firstValue);
+    console.log("secondValue is " + secondValue);
+    display.textContent = operate(operator, firstValue, secondValue);
+    firstValue = parseInt(display.textContent);
+});
+
+const clearBtn = document.querySelector('#clear');
+clearBtn.addEventListener('click', () => {
+    display.textContent = '';
+    numHasBeenClicked = false;
+    operator = '';
+    opHasBeenClicked = false;
+    firstValue = 0;
+    secondValue = 0;
+});
+
+
+//TODO: Add animation for click events
+
+//TODO: string operations together
+//TODO: 
